62a6d4ced36742ba3534e8e83c769a48af7baca6
patrizia <patriziagessa@gmail.com>
2020-04-09 18:12:13 +0200

    Reflaction

--- src/academy/boostprogramme/java/week5/reflaction/exercise2/LinesCalculatorTest.java
+++ src/academy/boostprogramme/java/week5/reflaction/exercise2/LinesCalculatorTest.java
@@ -10,42 +10,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class LinesCalculatorTest {
 
     LinesCalculator calculator = new LinesCalculator();
-
     @Test
     void calculateReturnsEmptyListForEmptyEntry() {
         List<String> input = List.of();
-
         List<String> result = calculator.calculate(input);
-
         Assertions.assertEquals(List.of(), result);
     }
-
     @Test
     void calculateReturnsEmptyListFor() {
         List<String> input = List.of("1");
-
         List<String> result = calculator.calculate(input);
-
         Assertions.assertEquals(List.of(), result);
     }
-
     @Test
     void calculateReturnsSumForPositiveString() {
         List<String> input = List.of("1,2,3,4","1234");
-
         List<String> result = calculator.calculate(input);
-
         Assertions.assertEquals(List.of("1244"), result);
     }
-
     @Test
     void calculateReturnsSumForNegativeAndPositiveString() {
         List<String> input = List.of("-2,-1,-4,3", "3,-4,5,6,-7");
-
         List<String> result = calculator.calculate(input);
-
         Assertions.assertEquals(List.of("-1"),result);
-
     }
 
 }
